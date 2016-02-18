0377749c1318ca90159a819d5b9f435783f08d26
Satoru Kanazawa <str314593@gmail.com>
2016-02-18 15:44:39 +0900

    課題2の再提出です。

--- src/Kisokadai2.java
+++ src/Kisokadai2.java
@@ -17,9 +17,12 @@ public class Kisokadai2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int answer;
-		System.out.println("数当てゲームです。１～１００までの数字を入力してください。");
-		System.out.println("１０００で終了します。");
+		int count = 0;
+		String finish = null;
+	while (true) {
 
+		System.out.println("数当てゲームです。１～１００の数字を入力してください。");
+		System.out.println("終了は１０００を入力してください。");
 		answer = new java.util.Random().nextInt(100);
 		//System.out.println(answer);
 
@@ -27,6 +30,7 @@ public class Kisokadai2 {
 		int input;
 
 			while (true) {
+				count++;
 				try {
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				str = br.readLine();
@@ -50,10 +54,44 @@ public class Kisokadai2 {
 					System.out.println("もっと小さな数字です。");
 					continue;
 				  }  else {
-					System.out.println("正解です！終了します。");
-					break;
+					  	if (count == 1) {
+					  		System.out.println("すごい！！一発で正解です！");
+					  		break;
+					  	} else {
+					  		System.out.println("正解です！終了します。");
+							System.out.println(count + "回目で正解しました。");
+							break;
+					  	}
+
 				  	}
 
+				}
+			System.out.println("もう一度やりますか？");
+			System.out.println("続ける y | やめる y");
+
+			try {
+				BufferedReader fin = new BufferedReader(new InputStreamReader(System.in));
+				finish = fin.readLine();
+
+					if (finish.equals("n")) {
+						System.out.println("Adios...");
+						break;
+					}
+					else if (finish.equals("y")) {
+						count = 0;
+						continue;
+					} else {
+						System.out.println("続ける y | やめる n");
+					}
+
+
+			}	catch (Exception e) {
+					e.printStackTrace();
+				}
+
 		}
+
+
+
 	}
 }
