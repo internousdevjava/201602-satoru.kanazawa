f5c3cba1d5257599d17d951ba00ddb2a85a4dd06
Satoru Kanazawa <str314593@gmail.com>
2016-02-18 14:48:39 +0900

    再提出です。

--- src/Kisokadai1.java
+++ src/Kisokadai1.java
@@ -16,36 +16,50 @@ public class Kisokadai1 {
 	 */
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("数値を入力");
 		String str = null;
-
-			try {
-				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				str = br.readLine();
-			}	catch (Exception e) {
-					e.printStackTrace();
-				}
-
-		int x = Integer.parseInt(str);
-
-		System.out.println("数値を入力");
 		String ver = null;
 
+	while (true) {
+				//縦の数字
+				int x;
+				System.out.println("縦の行の桁数を入力してください。１～１００");
+				try {
+					BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+					str = br.readLine();
+					x = Integer.parseInt(str);
+				}	catch (Exception e) {
+					System.out.println("数字を入力してください。");
+					continue;
+				}
+
+		//横の行
+		System.out.println("横の行の桁数を入力してください。１～１００");
+
+		int y;
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			ver = br.readLine();
+			y = Integer.parseInt(ver);
 		}	catch (Exception e) {
-				e.printStackTrace();
+			System.out.println("2数字を入力してください。");
+			continue;
 			}
 
-		int y = Integer.parseInt(ver);
-
-		for (int i = 1; i <= x; i++) {
-			for (int j = 1; j <= y; j++) {
-				System.out.print(j * i + " ");
+			if (x >= 1 && x <= 100 && y >= 1 && y <= 100) {
+				for (int i = 1; i <= x; i++) {
+					for (int j = 1; j <= y; j++) {
+						System.out.print((i*j)>=10 ? " "+(i*j) : "  "+(i*j));
+					}
+						System.out.println();
+				}
+			} else {
+				System.out.println("エラーです。");
+				continue;
 			}
-				System.out.println();
-		}
+			break;
+
+	}
+
 
 	}
 
--- src/Kisokadai2.java
+++ src/Kisokadai2.java
@@ -18,30 +18,42 @@ public class Kisokadai2 {
 		// TODO 自動生成されたメソッド・スタブ
 		int answer;
 		System.out.println("数当てゲームです。１～１００までの数字を入力してください。");
+		System.out.println("１０００で終了します。");
 
 		answer = new java.util.Random().nextInt(100);
 		//System.out.println(answer);
 
 		String str = null;
+		int input;
 
-		try {
 			while (true) {
+				try {
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 				str = br.readLine();
-				int input = Integer.parseInt(str);
-				if ( answer > input ) {
+				input = Integer.parseInt(str);
+				}
+				catch (Exception e) {
+					System.out.println("数字を入力してください。");
+					continue;
+				}
+				  if ( input == 1000 ){
+					  System.out.println("終了します。");
+					  break;
+				  } else if (input < 1 || input > 100){
+					  System.out.println("１～１００の数字を入力してください。");
+					  continue;
+				  }
+				  else if ( answer > input ) {
 					System.out.println("もっと大きな数字です。");
 					continue;
 				} else if ( input > answer ) {
 					System.out.println("もっと小さな数字です。");
 					continue;
-				  } else {
-					System.out.println("正解です！");
+				  }  else {
+					System.out.println("正解です！終了します。");
 					break;
 				  	}
-			}
-		}		catch (Exception e) {
-				e.printStackTrace();
-				}
+
+		}
 	}
 }
